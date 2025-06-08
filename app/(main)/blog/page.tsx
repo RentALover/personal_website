79f3d79dcfa4035225032433b09a38a237269e12
zyz <YuzhaoYj@outlook.com>
2025-06-08 15:01:16 +0800

    修改博客页面文字内容和格式

--- app/(main)/blog/page.tsx
+++ app/(main)/blog/page.tsx
@@ -5,17 +5,23 @@ import { Container } from '~/components/ui/Container'
 
 import { BlogPosts } from './BlogPosts'
 
-const description =
-  '写博客文章是我比较喜欢的沉淀分享方式，我希望能够把好用的技术知识传递给更多的人。我比较喜欢围绕着技术为主的话题，但是也会写一些非技术的话题，比如设计、创业、企业管理、生活随笔等等。'
+const description = (
+  <>
+    留住 温度 速度 温柔和愤怒<br />
+    凝住 今日 怎样 好<br />
+    捉紧 生命浓度 坦白流露 感情和态度<br />
+    留下 浮光 掠影 飞舞<br />
+  </>
+)
 export const metadata = {
-  title: '我的博客',
+  title: 'My Blog',
   description,
   openGraph: {
-    title: '我的博客',
+    title: 'My Blog',
     description,
   },
   twitter: {
-    title: '我的博客',
+    title: 'My Blog',
     description,
     card: 'summary_large_image',
   },
@@ -27,7 +33,7 @@ export default function BlogPage() {
     <Container className="mt-16 sm:mt-24">
       <header className="max-w-2xl">
         <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-          欢迎光临我的博客
+          Welcome to My Blog!
         </h1>
         <p className="my-6 text-base text-zinc-600 dark:text-zinc-400">
           <Balancer>{description}</Balancer>
